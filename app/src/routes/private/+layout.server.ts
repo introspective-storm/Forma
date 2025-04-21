22059bc733e0bdd013d5fed330ae7eb545186a13
Anikaith Bhardwaj <anikaith198@gmail.com>
2025-04-20 21:32:28 -0600

    added supabase auth

---
+++ app/src/routes/private/+layout.server.ts
@@ -0,0 +1,6 @@
+/**
+ * This file is necessary to ensure protection of all routes in the `private`
+ * directory. It makes the routes in this directory _dynamic_ routes, which
+ * send a server request, and thus trigger `hooks.server.ts`.
+ **/
+
